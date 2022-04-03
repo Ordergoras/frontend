936ed31f45a0ee4443646dbf8b085e1f69b8cd5b
Felix Wagner <felixwagner99@googlemail.com>
2022-04-03 22:01:33 +0200

    cleanup test queries

--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -2,23 +2,79 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Paper, Typography } from '@mui/material';
 import { useAppSelector, useAppDispatch } from '../Redux/hooks'
-import {login, logout} from "../Redux/authSlice";
+import { login, logout, selectAuth } from '../Redux/authSlice';
+import { createRequest } from '../utils/fetchUtils';
+
+const styles = {
+  button: {
+
+  },
+}
+
+export const jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdGFmZklkIjoiMWFkNjBmOGEzMzFmNDg0NWJkNmU3YWEyNzMwZTdhOGMifQ.9cp1e4sJbMu3ah-mmGFW-Mq_6Z3b7wcmO0RLNPXgzE0"
 
 function HomePage() {
-  const styles = {
-    homePage: {
-      height: '100%',
-      width: '100%',
-    },
-    button: {
-    },
-  }
 
   const dispatch = useAppDispatch()
-  const authState = useAppSelector(state => state.auth);
+  const authState = useAppSelector(selectAuth)
+
+  const getItems = () => {
+    createRequest('PUT', 'storage/getItems', {itemIds: [1,2,3]})
+      .then(res => {
+        if(res) res.json().then(data => console.log(data))
+      })
+      .catch((e) => {alert(e)})
+  }
+
+  const registerStaff = () => {
+    createRequest('POST', 'staff/registerStaff', {name: 'Monargoras', password: 'password'})
+      .then(res => {
+        if(res) res.json().then(data => {
+          if(res.ok)
+            console.log(data)
+          else
+            alert(data.message)
+        })
+      })
+      .catch((e) => {alert(e)})
+  }
+
+  const loginStaff = () => {
+    createRequest('POST', 'staff/login', {name: 'Monargoras', password: 'password'})
+      .then(res => {
+        if(res) res.json().then(data => {
+          if(res.ok)
+            dispatch(login({token: data.jwtToken, name: data.name}))
+          else
+            alert(data.message)
+        })
+      })
+      .catch((e) => {alert(e)})
+  }
+
+  const logoutStaff = () => {
+    createRequest('POST', 'staff/logout', undefined, undefined)
+      .then(res => {
+        if(res)
+          res.json().then(data => {
+            alert(data.message)
+            if(res.ok)
+              dispatch(logout())
+          })
+      })
+      .catch((e) => {alert(e)})
+  }
+
+  const getStaff = () => {
+    createRequest('GET', 'staff/getStaff', undefined, {staffId: '1ad60f8a331f4845bd6e7aa2730e7a8c'})
+      .then(res => {
+        if(res && res.ok) res.json().then(data => console.log(data))
+      })
+      .catch((e) => {console.log(e)})
+  }
 
   return (
-    <div style={styles.homePage}>
+    <div>
       <Typography>Welcome to Ordergoras!</Typography>
       <Paper>
         <Typography>
@@ -29,7 +85,7 @@ function HomePage() {
         sx={styles.button}
         color={'primary'}
         variant={'contained'}
-        onClick={() => dispatch(login({staffId: 1, name: 'Monargoras'}))}
+        onClick={() => loginStaff()}
       >
         Login
       </Button>
@@ -37,9 +93,33 @@ function HomePage() {
         sx={styles.button}
         color={'secondary'}
         variant={'contained'}
-        onClick={() => dispatch(logout())}
+        onClick={() => logoutStaff()}
       >
         Logout
+      </Button>
+      <Button
+        sx={styles.button}
+        color={'primary'}
+        variant={'contained'}
+        onClick={() => registerStaff()}
+      >
+        register User
+      </Button>
+      <Button
+        sx={styles.button}
+        color={'secondary'}
+        variant={'contained'}
+        onClick={() => getItems()}
+      >
+        get Items
+      </Button>
+      <Button
+        sx={styles.button}
+        color={'primary'}
+        variant={'contained'}
+        onClick={() => getStaff()}
+      >
+        get Staff
       </Button>
       <Link to={'/about'}><Typography>About</Typography></Link>
     </div>
