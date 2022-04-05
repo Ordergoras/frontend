3730956eeef71d48f4f08e3dc2a464f51b671f2f
Felix Wagner <felixwagner99@googlemail.com>
2022-04-05 14:49:49 +0200

    add general style file

---
+++ src/styles/generalStyles.tsx
@@ -0,0 +1,11 @@
+export const generalStyles = {
+  backgroundContainer: {
+    background: 'linear-gradient(to right bottom, #430089, #82ffa1)',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignContent: 'center',
+  },
+}
