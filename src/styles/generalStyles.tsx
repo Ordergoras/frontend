b97402faf05aeb342632af2316729a07c12eb7bd
Felix Wagner <felixwagner99@googlemail.com>
2022-04-05 21:59:49 +0200

    add login prompt

--- src/styles/generalStyles.tsx
+++ src/styles/generalStyles.tsx
@@ -1,11 +1,28 @@
+export const utils = {
+  topBottomMargin: {
+    marginTop: 1,
+    marginBottom: 1,
+  },
+}
+
 export const generalStyles = {
   backgroundContainer: {
-    background: 'linear-gradient(to right bottom, #430089, #82ffa1)',
+    background: 'linear-gradient(to right bottom, #FF7043, #FFAB40)',
     height: '100vh',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     textAlign: 'center',
     alignContent: 'center',
+  },
+  textField: {
+    ...utils.topBottomMargin,
+  },
+  paper: {
+    padding: 8,
+    margin: 'auto',
+  },
+  button: {
+    ...utils.topBottomMargin,
   },
 }
