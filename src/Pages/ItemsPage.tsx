98ea8861fa668c45d552620d7ba259bdac0307ed
Felix Wagner <felixwagner99@googlemail.com>
2022-04-19 00:40:32 +0200

    adjust itemsPage to fit with other pages

--- src/Pages/ItemsPage.tsx
+++ src/Pages/ItemsPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Paper } from '@mui/material';
 import { useAppSelector } from '../Redux/hooks';
 import { selectData } from '../Redux/dataSlice';
 import ItemCard from '../OrderComponents/ItemCard';
@@ -10,6 +10,11 @@ import { useTranslation } from 'react-i18next';
 function ItemsPage() {
 
   const styles = {
+    paper: {
+      padding: 2,
+      margin: 1,
+      backgroundColor: theme.palette.background.default,
+    },
     topMargin: {
       marginTop: 1,
     },
@@ -20,51 +25,57 @@ function ItemsPage() {
 
   return (
     <Box sx={{textAlign: 'center', paddingTop: 1, paddingBottom: 1}}>
-      <Typography variant={'h4'} color={theme.palette.background.default} sx={styles.topMargin}>
-        {t('drinks')}
-      </Typography>
-      <Box sx={generalStyles.flexWrapBox}>
-        {
-          dataState.drinks !== undefined &&
+      <Paper sx={styles.paper}>
+        <Typography variant={'h4'} sx={styles.topMargin}>
+          {t('drinks')}
+        </Typography>
+        <Box sx={generalStyles.flexWrapBox}>
+          {
+            dataState.drinks !== undefined &&
             dataState.drinks.map((item) => {
               return <ItemCard key={item.itemId} item={item} color={theme.palette.primary.light}/>
             })
-        }
-        {
-          dataState.drinks === undefined &&
-            <Typography variant={'body1'} color={theme.palette.background.default}>{t('nothingFound')}</Typography>
-        }
-      </Box>
-      <Typography variant={'h4'} color={theme.palette.background.default} sx={styles.topMargin}>
-        {t('food')}
-      </Typography>
-      <Box sx={generalStyles.flexWrapBox}>
-        {
-          dataState.food !== undefined &&
+          }
+          {
+            dataState.drinks === undefined &&
+              <Typography variant={'body1'}>{t('nothingFound')}</Typography>
+          }
+        </Box>
+      </Paper>
+      <Paper sx={styles.paper}>
+        <Typography variant={'h4'} sx={styles.topMargin}>
+          {t('food')}
+        </Typography>
+        <Box sx={generalStyles.flexWrapBox}>
+          {
+            dataState.food !== undefined &&
             dataState.food.map((item) => {
               return <ItemCard key={item.itemId} item={item} color={theme.palette.primary.main}/>
             })
-        }
-        {
-          dataState.food === undefined &&
-            <Typography variant={'body1'} color={theme.palette.background.default}>{t('nothingFound')}</Typography>
-        }
-      </Box>
-      <Typography variant={'h4'} color={theme.palette.background.default} sx={styles.topMargin}>
-        {t('other')}
-      </Typography>
-      <Box sx={generalStyles.flexWrapBox}>
-        {
-          dataState.other !== undefined &&
+          }
+          {
+            dataState.food === undefined &&
+              <Typography variant={'body1'}>{t('nothingFound')}</Typography>
+          }
+        </Box>
+      </Paper>
+      <Paper sx={styles.paper}>
+        <Typography variant={'h4'} sx={styles.topMargin}>
+          {t('other')}
+        </Typography>
+        <Box sx={generalStyles.flexWrapBox}>
+          {
+            dataState.other !== undefined &&
             dataState.other.map((item) => {
               return <ItemCard key={item.itemId} item={item} color={theme.palette.primary.dark}/>
             })
-        }
-        {
-          dataState.other === undefined &&
-            <Typography variant={'body1'} color={theme.palette.background.default}>{t('nothingFound')}</Typography>
-        }
-      </Box>
+          }
+          {
+            dataState.other === undefined &&
+              <Typography variant={'body1'}>{t('nothingFound')}</Typography>
+          }
+        </Box>
+      </Paper>
     </Box>
   )
 }
