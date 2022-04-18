83fbee10e24b64af732a29f059bb62cb9c706c14
Felix Wagner <felixwagner99@googlemail.com>
2022-04-19 00:06:14 +0200

    add clickable item card

--- src/Pages/CreateOrderPage.tsx
+++ src/Pages/CreateOrderPage.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Typography, Box, Accordion, AccordionSummary, AccordionDetails, Chip, Paper } from '@mui/material';
 import { generalStyles } from '../styles/generalStyles';
 import { theme } from '../index';
-import ItemCard from '../OrderComponents/ItemCard';
 import { useAppSelector } from '../Redux/hooks';
 import { selectData } from '../Redux/dataSlice';
 import { useTranslation } from 'react-i18next';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Order, Item } from '../utils/types';
 import { selectAuth } from '../Redux/authSlice';
+import ClickableItem from '../OrderComponents/ClickableItem';
 
 function CreateOrderPage() {
 
@@ -73,12 +73,16 @@ function CreateOrderPage() {
 
   return (
     <Box sx={{textAlign: 'center', margin: 1}}>
-      <Paper sx={{padding: 1, marginBottom: 2}}>
+      <Paper sx={{padding: 1, marginBottom: 2, minHeight: '10vh'}}>
         {Object.keys(order.orderedItems).map((itemId) => {
           return dataState.itemIdMap &&
             <Chip
               key={itemId}
-              sx={{...styles.chip, backgroundColor: theme.palette.secondary.dark}}
+              sx={{
+                ...styles.chip,
+                backgroundColor: dataState.itemIdMap[itemId]['group'] === 'Drink' ? theme.palette.primary.light :
+                  dataState.itemIdMap[itemId]['group'] === 'Food' ? theme.palette.primary.main : theme.palette.primary.dark
+            }}
               label={dataState.itemIdMap[itemId]['name'] + ': ' + order.orderedItems[itemId]}
               onClick={() => {
                 if(dataState.itemIdMap)
@@ -104,9 +108,12 @@ function CreateOrderPage() {
             {
               dataState.drinks !== undefined &&
               dataState.drinks.map((item) => {
-                return <Box key={item.itemId} onClick={() => addItemToOrder(item)}>
-                  <ItemCard item={item} color={theme.palette.primary.light}/>
-                </Box>
+                return <ClickableItem
+                  key={item.itemId}
+                  item={{...item, amount: item.amount - (isNaN(order.orderedItems[item.itemId]) ? 0 : order.orderedItems[item.itemId])}}
+                  color={theme.palette.primary.light}
+                  onClick={() => addItemToOrder(item)}
+                />
               })
             }
             {
@@ -127,9 +134,12 @@ function CreateOrderPage() {
             {
               dataState.food !== undefined &&
               dataState.food.map((item) => {
-                return <Box key={item.itemId} onClick={() => addItemToOrder(item)}>
-                  <ItemCard item={item} color={theme.palette.primary.main}/>
-                </Box>
+                return <ClickableItem
+                  key={item.itemId}
+                  item={{...item, amount: item.amount - (isNaN(order.orderedItems[item.itemId]) ? 0 : order.orderedItems[item.itemId])}}
+                  color={theme.palette.primary.main}
+                  onClick={() => addItemToOrder(item)}
+                />
               })
             }
             {
@@ -150,9 +160,12 @@ function CreateOrderPage() {
             {
               dataState.other !== undefined &&
               dataState.other.map((item) => {
-                return <Box key={item.itemId} onClick={() => addItemToOrder(item)}>
-                  <ItemCard item={item} color={theme.palette.primary.dark}/>
-                </Box>
+                return <ClickableItem
+                  key={item.itemId}
+                  item={{...item, amount: item.amount - (isNaN(order.orderedItems[item.itemId]) ? 0 : order.orderedItems[item.itemId])}}
+                  color={theme.palette.primary.dark}
+                  onClick={() => addItemToOrder(item)}
+                />
               })
             }
             {
