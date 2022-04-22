3251c0210c7fd2a4f5d865c967003b7cd942e1c4
Felix Wagner <felixwagner99@googlemail.com>
2022-04-23 00:16:43 +0200

    add admin page and first item draft

---
+++ src/OrderComponents/AdminItem.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Box, Button, Grid, Paper } from '@mui/material';
+import { Item } from '../utils/types';
+import { theme } from '../index';
+
+interface AdminItemProps {
+  item: Item,
+  color: string,
+}
+
+function AdminItem(props: AdminItemProps) {
+
+  const styles = {
+    button: {
+      width: '100%',
+      textTransform: 'none',
+    },
+    paper: {
+      backgroundColor: props.item.amount > 0 ? props.color : theme.palette.error.dark,
+      width: '100%',
+      padding: 1,
+      display: 'flex',
+      flexDirection: 'row',
+    },
+  }
+
+  return (
+    <Button sx={styles.button}>
+      <Paper sx={styles.paper}>
+        <Grid container>
+          <Grid item xs={6}>
+            <Box sx={{textAlign: 'start'}}>
+              { props.item.name }
+            </Box>
+          </Grid>
+          <Grid item xs={3}>
+            <Box>
+              { props.item.amount }
+            </Box>
+          </Grid>
+          <Grid item xs={3}>
+            <Box sx={{textAlign: 'end'}}>
+              { props.item.price.toFixed(2) + '€' }
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Button>
+  )
+}
+
+export default AdminItem;
---
+++ src/Pages/AdminPage.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Grid, Paper, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useAppSelector } from '../Redux/hooks';
+import { selectAuth } from '../Redux/authSlice';
+import { selectData } from '../Redux/dataSlice';
+import { useTranslation } from 'react-i18next';
+import AdminItem from '../OrderComponents/AdminItem';
+import { theme } from '../index';
+import { Item } from '../utils/types';
+
+function AdminPage() {
+
+  const styles = {
+    accordionTitle: {
+      width: '100%',
+      flexShrink: 0,
+    },
+  }
+
+  const getAllItems = (): Item[] => {
+    let items: Item[] = []
+    if(dataState.drinks)
+      items.push(...dataState.drinks)
+    if(dataState.food)
+      items.push(...dataState.food)
+    if(dataState.other)
+      items.push(...dataState.other)
+    return items
+  }
+
+  const authState = useAppSelector(selectAuth)
+  const dataState = useAppSelector(selectData)
+  const { t } = useTranslation()
+  const [expanded, setExpanded] = React.useState<string | false>('items')
+  const [items, setItems] = React.useState<Item[]>(getAllItems)
+  const [sortKey, setSortKey] = React.useState(undefined)
+  const [sortAsc, setSortAsc] = React.useState(true)
+
+  React.useEffect(() => {
+    if(sortKey === 'amount' && sortAsc) {
+      setItems([...items].sort((i1, i2) => i1.amount - i2.amount))
+    } else if(sortKey === 'amount' && !sortAsc) {
+      setItems([...items].sort((i1, i2) => i2.amount - i1.amount))
+    } else if(sortKey === 'price' && sortAsc) {
+      setItems([...items].sort((i1, i2) => i1.price - i2.price))
+    } else if(sortKey === 'price' && !sortAsc) {
+      setItems([...items].sort((i1, i2) => i2.price - i1.price))
+    }
+  }, [items, sortAsc, sortKey])
+
+  const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false)
+  }
+
+  return (
+    <Box sx={{textAlign: 'center', margin: 1}}>
+      <Accordion expanded={expanded === 'items'} onChange={handleChange('items')}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={styles.accordionTitle}>
+            {t('itemDash')}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Paper sx={{padding: 1, margin: 1, display: 'flex', flexDirection: 'row'}}>
+            <Grid container>
+              <Grid item xs={6}>
+                <Box sx={{textAlign: 'start'}}>
+                  {t('name')}
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box>
+                  {t('inStorage')}
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box sx={{textAlign: 'end'}}>
+                  {t('price')}
+                </Box>
+              </Grid>
+            </Grid>
+          </Paper>
+          {
+            items.map((item) => {
+              return <AdminItem
+                item={item}
+                color={item.group === 'Drink' ? theme.palette.primary.light : item.group === 'Food' ? theme.palette.primary.main : theme.palette.primary.dark}
+              />
+            })
+          }
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'staff'} onChange={handleChange('staff')}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography sx={styles.accordionTitle}>
+            {t('staffDash')}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Staff  Overview
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  )
+}
+
+export default AdminPage;
