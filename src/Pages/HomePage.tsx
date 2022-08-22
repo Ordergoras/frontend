8dcc08a13ced0015aa0682884efd80d849cdff5e
Felix Wagner <felixwagner99@googlemail.com>
2022-08-23 01:24:33 +0200

    moved todos into github issues

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -23,12 +23,6 @@ function HomePage() {
     <Box sx={generalStyles.backgroundContainer}>
       <Box>
         <Typography color={theme.palette.background.default} variant={'h5'}>{t('welcome', {name: authState.name})}</Typography>
-        <Typography color={theme.palette.background.default} variant={'h5'}>TODO</Typography>
-        <Typography color={theme.palette.background.default} variant={'body1'}>Fix wine orders (full bottle vs. glass), try: save stock thats sold in full and parts as the parts (e.g. 1 bottle wine is saved as 5 units (1L in 0.2L glasses))</Typography>
-        <Typography color={theme.palette.background.default} variant={'body1'}>add additional information to food and wine (item cards)</Typography>
-        <Typography color={theme.palette.background.default} variant={'body1'}>statistics dashboard</Typography>
-        <Typography color={theme.palette.background.default} variant={'body1'}>logging of actions (to potentially revert bad inputs by staff)</Typography>
-        <Typography color={theme.palette.background.default} variant={'body1'}>add staff,admin,superuser groups instead of isAdmin bool</Typography>
       </Box>
       <Link to={'/license'}><Typography>{t('licensePage')}</Typography></Link>
     </Box>
